d610ab935d168885859cf69c747705b3a6d61742
BintangXD112 <103170567+BintangXD112@users.noreply.github.com>
2025-07-04 14:46:49 +0700

    input: add header for keyboard input and navigation

---
+++ input.h
@@ -0,0 +1,20 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+int editorReadKey();
+void editorProcessKeypress();
+void editorMoveCursor(int key);
+
+enum editorKey {
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN,
+    DEL_KEY,
+    HOME_KEY,
+    END_KEY,
+    PAGE_UP,
+    PAGE_DOWN
+};
+
+#endif // INPUT_H 
